b93093a25a6e1da914e90ddb912cbde7a6525741
Dikfield <dinash@outlook.comm>
2025-07-15 18:53:59 +0200

    Adding Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { ClientList } from '../features/clients/client-list/client-list';
+import { ClientDetailed } from '../features/clients/client-detailed/client-detailed';
+import { Lists } from '../features/lists/lists';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'clients', component: ClientList },
+      { path: 'clients/:id', component: ClientDetailed },
+      { path: 'lists', component: Lists },
+    ],
+  },
+  { path: '**', component: Home },
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,41 +1,13 @@
-import { AccountService } from './../core/services/account-service';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { Home } from '../features/home/home';
-import { Client } from '../types/client';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'Viva Tubismo';
-  protected clients = signal<Client[]>([]);
-
-  async ngOnInit() {
-    this.setCurrentUser();
-    this.clients.set(await this.getClients());
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getClients() {
-    try {
-      return lastValueFrom(this.http.get<Client[]>('https://localhost:5001/api/clients'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
